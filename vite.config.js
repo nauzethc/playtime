c544b3c9df9c3e9c8b29982443bc59009b12b92b
Nauzet Hernandez <nauzet.hernandez@tng.iac.es>
2021-11-24 09:31:47 +0000

    feat: PWA configuration

--- vite.config.js
+++ vite.config.js
@@ -6,9 +6,27 @@ export default defineConfig({
   plugins: [
     vue(),
     VitePWA({
-      workbox: {
-        sourcemap: true,
-        globPatterns: ['src/assets/svg/*.svg']
+      mode: process.env.MODE || 'development',
+      base: '/',
+      includeAssets: ['favicon.ico'],
+      manifest: {
+        name: 'PlayTime',
+        short_name: 'PlayTime',
+        theme_color: '#1f2937',
+        background_color: '#1f2937',
+        display: 'standalone',
+        icons: [
+          {
+            src: '/pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
       }
     })
   ],
