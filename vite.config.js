99e160d271b73f997c62a29467c340f256e24784
Nauzet Hernandez <nauzet.hernandez@tng.iac.es>
2021-11-24 13:07:54 +0000

    fix: API url changes between dev and prod

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,15 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { VitePWA } from 'vite-plugin-pwa'
 
+const IS_DEVEL = process.env.MODE === 'development'
+const PORT = process.env.API_PORT || 4000
+
 export default defineConfig({
+  define: {
+    API_BASE_URL: IS_DEVEL
+      ? JSON.stringify(`http://localhost:${PORT}/api`)
+      : JSON.stringify('http://www.somehost.com/api')
+  },
   plugins: [
     vue(),
     VitePWA({
@@ -29,12 +37,5 @@ export default defineConfig({
         ]
       }
     })
-  ],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:4000'
-      }
-    }
-  }
+  ]
 })
