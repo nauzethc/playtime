647a9a71e7ff034a28222a879abdbd2e97668ae0
Nauzet Hernandez <nauzet.hernandez@tng.iac.es>
2021-11-24 13:36:05 +0000

    feat: basic API with netlify functions

--- functions/games.js
+++ functions/games.js
@@ -1,8 +1,32 @@
+const { find, get } = require('howlongtobeat-api')
+const PATH = '/.netlify/functions/games'
+
 exports.handler = async function (event, context) {
-  console.log(event.path)
-  console.log(event.queryStringParameters)
-  return {
-    statusCode: 200,
-    body: JSON.stringify('Hello, world!')
+  try {
+    if (event.path === PATH) {
+      const games = await find({ ...event.queryStringParameters })
+      return {
+        statusCode: 200,
+        body: JSON.stringify(games)
+      }
+    } else if (event.path.startsWith(`${PATH}/`)) {
+      const id = parseInt(event.path.replace(`${PATH}/`, ''))
+      const game = await get(id)
+      return {
+        statusCode: game ? 200 : 404,
+        body: game ? JSON.stringify(game) : 'Game not found'
+      }
+    } else {
+      return {
+        statusCode: 400,
+        body: 'Invalid URL'
+      }
+    }
+  } catch (err) {
+    console.log(err)
+    return {
+      statusCode: 500,
+      body: JSON.stringify(err)
+    }
   }
 }
